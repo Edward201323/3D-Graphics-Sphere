3e31f85c61a95f0f7aa38057ddbc881af1db8f0d
Edward Song <essanuk2013@gmail.com>
2025-02-21 11:47:05 -0800

    Working triangle

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame as pg
 from OpenGL.GL import *
 import numpy as np
 import ctypes
+from OpenGL.GL.shaders import compileProgram, compileShader
 
 class App:
 
@@ -10,8 +11,24 @@ class App:
         pg.display.set_mode((640, 400), pg.OPENGL | pg.DOUBLEBUF)
         self.clock = pg.time.Clock()
         glClearColor(0.1, 0.2, 0.2, 1)
+        self.shader = self.createShader("shaders/vertex.txt", "shaders/fragment.txt")
+        glUseProgram(self.shader)
+        self.triangle = Triangle()
         self.mainLoop()
 
+    def createShader(self, vertexFilepath, fragmentFilepath):
+        with open(vertexFilepath, 'r') as f:
+            vertex_src = f.readlines()
+        
+        with open(fragmentFilepath, 'r') as f:
+            fragment_src = f.readlines()
+
+        shader = compileProgram(
+            compileShader(vertex_src, GL_VERTEX_SHADER),
+            compileShader(fragment_src, GL_FRAGMENT_SHADER)
+        )
+
+        return shader
 
     def mainLoop(self):
         running = True
@@ -20,13 +37,21 @@ class App:
                 if(event.type == pg.QUIT):
                     running = False
 
-            glClear(GL_COLOR_BUFFER_BIT)
+            glClear(GL_COLOR_BUFFER_BIT) #refreshes screen
+
+            glUseProgram(self.shader)
+            glBindVertexArray(self.triangle.vao)
+            glDrawArrays(GL_TRIANGLES, 0, self.triangle.vertex_count)
+
             pg.display.flip()
 
             self.clock.tick(60)
         self.quit()
 
     def quit(self):
+
+        self.triangle.destroy()
+        glDeleteProgram(self.shader)
         pg.quit()
 
 class Triangle:
@@ -36,8 +61,8 @@ class Triangle:
         #x, y, z, r, g, b
         self.vertices = (
             -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-            -0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-            -0.5, -0.5, 0.0, 0.0, 0.0, 1.0
+             0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+             0.0,  0.5, 0.0, 0.0, 0.0, 1.0
         )
 
         self.vertices = np.array(self.vertices, dtype=np.float32) # The second argument determines the datatype stored in the vertices
@@ -50,10 +75,12 @@ class Triangle:
         glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
         glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
 
-        glEnableVertexArrayAttrib(0)
+        glEnableVertexAttribArray(0)
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+
         glEnableVertexAttribArray(1)
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+
 
     def destroy(self):
 
